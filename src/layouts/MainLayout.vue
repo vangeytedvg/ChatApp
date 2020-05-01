72783f33ba20633a553d60eba97368a7d4456d9e
vangeytedvg <vangeytedvg@gmail.com>
2020-05-01 20:26:25 +0200

    First Release

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,86 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-toolbar-title class="absolute-center">{{ title.toUpperCase() }}</q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script>
+import EssentialLink from "components/EssentialLink";
+
+export default {
+  name: "MainLayout",
+
+  components: {
+    EssentialLink
+  },
+  computed: {
+    // This function returns the path were located at
+    title() {
+      let myPath = this.$route.fullPath
+      if (myPath == '/') {
+        return 'Smack Chat Main'
+      } else if (myPath == "/chat") {
+        return 'Chat'
+      } else if (myPath == '/auth') {
+        return 'Auth'
+      }
+    }
+  },
+  data() {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: [
+        {
+          title: "Docs",
+          caption: "quasar.dev",
+          icon: "school",
+          link: "https://quasar.dev"
+        },
+        {
+          title: "Github",
+          caption: "github.com/quasarframework",
+          icon: "code",
+          link: "https://github.com/quasarframework"
+        },
+        {
+          title: "Discord Chat Channel",
+          caption: "chat.quasar.dev",
+          icon: "chat",
+          link: "https://chat.quasar.dev"
+        },
+        {
+          title: "Forum",
+          caption: "forum.quasar.dev",
+          icon: "record_voice_over",
+          link: "https://forum.quasar.dev"
+        },
+        {
+          title: "Twitter",
+          caption: "@quasarframework",
+          icon: "rss_feed",
+          link: "https://twitter.quasar.dev"
+        },
+        {
+          title: "Facebook",
+          caption: "@QuasarFramework",
+          icon: "public",
+          link: "https://facebook.quasar.dev"
+        },
+        {
+          title: "Quasar Awesome",
+          caption: "Community Quasar projects",
+          icon: "favorite",
+          link: "https://awesome.quasar.dev"
+        }
+      ]
+    };
+  }
+};
+</script>
