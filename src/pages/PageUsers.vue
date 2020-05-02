3778f4036ec1734c7cdaf672977f6c8c6952fcec
vangeytedvg <vangeytedvg@gmail.com>
2020-05-02 12:47:14 +0200

    Simple Commit

--- src/pages/PageUsers.vue
+++ src/pages/PageUsers.vue
@@ -1,14 +1,52 @@
 <template>
-  <q-page class="flex">
-    <h5>Users page</h5>
+  <q-page class="flex q-pa-md">
+    <q-list class="full-width" separator>
+      <q-item v-for="user in users" :key="user.id" clickable v-ripple>
+        <q-item-section avatar>
+          <q-avatar color="primary" text-color="white">{{ user.name.charAt(0) }}</q-avatar>
+        </q-item-section>
+
+        <q-item-section>
+          <q-item-label>{{ user.name }}</q-item-label>
+        </q-item-section>
+
+        <q-item-section side>
+          <q-badge :color="user.online ? 'light-green-5' : 'grey-4'">
+            {{ user.online ? "Onine" : "Offline"}}
+          </q-badge>
+        </q-item-section>
+      </q-item>
+
+      <q-separator />
+    </q-list>
   </q-page>
 </template>
 
 <script>
 export default {
-}
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Danny",
+          online: true
+        },
+        {
+          id: 2,
+          name: "Ronny",
+          online: false
+        },
+        {
+          id: 3,
+          name: "Denka",
+          online: true
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
